b802326bf80a6dee1cce5f9ca45c0f1a58968f88
matthewcm <mc640@kent.ac.uk>
2021-03-07 18:18:23 +0000

    Refactoring

--- src/main/kotlin/SmartCalculator.kt
+++ src/main/kotlin/SmartCalculator.kt
@@ -4,75 +4,8 @@ import java.lang.Exception
 import java.util.*
 
 class SmartCalculator {
-
     private var variables = mutableMapOf<String, Int>()
-
     private var sign = 1
-
-    private fun subtractNextOperand(): Int {
-        sign = -1
-        return 0
-    }
-
-    private fun addNextOperand(): Int {
-        sign = 1
-        return 0
-    }
-
-    private fun performOperationOnOperand(num: Int): Int {
-        val result = sign * num
-        sign = 1
-        return result
-    }
-
-    private fun String.isNumber() : Boolean {
-        return if (isNullOrEmpty()) false else all { Character.isDigit(it) }
-    }
-
-    private fun variableAssignment(line: String) {
-
-        val (i, v) = line.split("=").map { it.trim() }
-
-        try {
-            if (i.isAVariable()){
-                if (variables.containsKey(v)) variables[i] = variables[v]!!.toInt()
-                else{
-                    if (v.isNumber()) variables[i] = v.toInt()
-                    else throw Exception()
-                }
-            }else throw Exception()
-
-        }catch (e: Exception){
-            println("Invalid assignment")
-        }
-
-    }
-
-    private fun calculateSumOfString(nums: String): Int {
-        return nums.split(" ").map { num ->
-            when {
-                num.isANegativeBinaryOperator() -> {
-                    if (num.length % 2 == 1) subtractNextOperand()
-                    else addNextOperand()
-                }
-                num.isAPositiveBinaryOperator() -> {
-                    addNextOperand()
-                }
-                num.isAVariable() -> {
-                    if (variables.containsKey(num)) performOperationOnOperand(variables[num]!!.toInt())
-                    else throw ArithmeticException()
-                }
-                else -> {
-                    performOperationOnOperand(num.toInt())
-                }
-
-            }
-        }.reduce { numSum, num -> numSum + num }
-    }
-
-    private fun String.isANegativeBinaryOperator(): Boolean = toSet().size == 1 && toSet().first() == '-'
-    private fun String.isAPositiveBinaryOperator(): Boolean = toSet().size == 1 && toSet().first() == '+'
-    private fun String.isAVariable(): Boolean = matches(Regex("[a-zA-Z]+"))
 
     fun sum(nums: String) {
         try {
@@ -84,30 +17,6 @@ class SmartCalculator {
         } catch (e: Exception) {
             println("Invalid Expression")
         }
-
-    }
-
-    private fun runCommand(command: String): Boolean {
-        when (command) {
-            "/exit" -> {
-                println("Bye!")
-                return false
-            }
-            "/help" -> {
-                println("The program calculates the sum of numbers")
-                println("Supports + and - operations.")
-                println("An even number of - is +. -- = +")
-            }
-            else -> println("Unknown Command")
-        }
-
-        return true
-
-    }
-
-    private fun printVariable(name: String) {
-        if (variables.containsKey(name)) println(variables[name])
-        else println("Unknown variable")
     }
 
     fun multiSum() {
@@ -135,6 +44,96 @@ class SmartCalculator {
             }
         }
 
+    }
+
+
+    private fun subtractNextOperand(): Int {
+        sign = -1
+        return 0
+    }
+
+    private fun addNextOperand(): Int {
+        sign = 1
+        return 0
+    }
+
+    private fun performOperationOnOperand(num: Int): Int {
+        val result = sign * num
+        sign = 1
+        return result
+    }
+
+    private fun calculateSumOfString(nums: String): Int {
+        return nums.split(" ").map { num ->
+            when {
+                num.isANegativeBinaryOperator() -> {
+                    if (num.length % 2 == 1) subtractNextOperand()
+                    else addNextOperand()
+                }
+                num.isAPositiveBinaryOperator() -> {
+                    addNextOperand()
+                }
+                num.isAVariable() -> {
+                    if (variables.containsKey(num)) performOperationOnOperand(variables[num]!!.toInt())
+                    else throw ArithmeticException()
+                }
+                else -> {
+                    performOperationOnOperand(num.toInt())
+                }
+
+            }
+        }.reduce { numSum, num -> numSum + num }
+    }
+
+
+    private fun variableAssignment(line: String) {
+
+        val (i, v) = line.split("=").map { it.trim() }
+
+        try {
+            if (i.isAVariable()){
+                if (variables.containsKey(v)) variables[i] = variables[v]!!.toInt()
+                else{
+                    if (v.isNumber()) variables[i] = v.toInt()
+                    else throw Exception()
+                }
+            }else throw Exception()
+
+        }catch (e: Exception){
+            println("Invalid assignment")
+        }
+
+    }
+
+
+    private fun runCommand(command: String): Boolean {
+        when (command) {
+            "/exit" -> {
+                println("Bye!")
+                return false
+            }
+            "/help" -> {
+                println("The program calculates the sum of numbers")
+                println("Supports + and - operations.")
+                println("An even number of - is +. -- = +")
+            }
+            else -> println("Unknown Command")
+        }
+
+        return true
+
+    }
+
+    private fun printVariable(name: String) {
+        if (variables.containsKey(name)) println(variables[name])
+        else println("Unknown variable")
+    }
+
+    private fun String.isANegativeBinaryOperator(): Boolean = toSet().size == 1 && toSet().first() == '-'
+    private fun String.isAPositiveBinaryOperator(): Boolean = toSet().size == 1 && toSet().first() == '+'
+    private fun String.isAVariable(): Boolean = matches(Regex("[a-zA-Z]+"))
+    private fun String.isNumber() : Boolean {
+        return if (isNullOrEmpty()) false else all { Character.isDigit(it) }
     }
 }
 
