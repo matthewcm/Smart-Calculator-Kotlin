3a63c61d3bb78d619a75c68b37230507fb3400a7
Matthew Castrillon-Madrigal <27matt37@gmail.com>
2021-03-07 02:17:54 +0000

    Multi ReadLine sum

--- src/main/kotlin/SmartCalculator.kt
+++ src/main/kotlin/SmartCalculator.kt
@@ -1,12 +1,38 @@
+import java.io.ByteArrayInputStream
 import java.util.*
 
 class SmartCalculator {
-    fun sum(): Int{
+
+    fun sum(a:Int, b:Int) = println(a + b)
+
+    fun multiSum() {
         val scanner = Scanner(System.`in`)
 
-        val a = scanner.nextInt()
-        val b = scanner.nextInt()
+        while (scanner.hasNextLine()){
+            var line = scanner.nextLine()
 
-        return a + b
+            if (line.toString() == "/exit") {
+                println("Bye!" )
+                return
+            }
+
+            if (line != ""){
+                val (x,y)  =  "$line 0"
+                    .split(' ')
+                    .map{it.toInt()}
+
+                sum(x, y)
+            }
+        }
+
     }
+}
+
+fun main () {
+    val input = "10 12\n5 6\n00"
+    val inp = ByteArrayInputStream(input.toByteArray())
+    System.setIn(inp)
+
+    val sc = SmartCalculator()
+    sc.multiSum()
 }
--- src/test/kotlin/SmartCalculatorTest.kt
+++ src/test/kotlin/SmartCalculatorTest.kt
@@ -3,42 +3,51 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 internal class SmartCalculatorTest {
 
     private var sc = SmartCalculator()
+    var out = ByteArrayOutputStream()
 
     @BeforeEach
     fun before() {
         sc = SmartCalculator()
+
+        out = ByteArrayOutputStream()
+        System.setOut(PrintStream(out))
     }
 
     @Test
     fun `Given '10 12' is input, When sum is invoked, then it should return 22`() {
 //         Given
-        val input = "5 6"
-        val inp = ByteArrayInputStream(input.toByteArray());
-        System.setIn(inp);
+        val x = 10
+        val y = 12
+
 
 //        WHEN
-        val sum = sc.sum()
+        sc.sum(x,y)
+        val sum = out.toString().trim().toInt()
 
 //        THEN
-        assertEquals(sum, 11)
+        assertEquals(22, sum)
 
     }
     @Test
-    fun `Given '5 6' is input, When sum is called, then it should return 11`() {
+    fun `Given '10 12' '5 6' as two inputs, When multiSum is invoked, then it should print 22 and print 11`() {
 //         Given
-        val input = "5 6"
+        val input = "10 12\n5 6\n"
         val inp = ByteArrayInputStream(input.toByteArray());
         System.setIn(inp);
 
 //        WHEN
-        val sum = sc.sum()
+        sc.multiSum()
+        var (a,b) = (out.toString().trim().split("\n").map{it.toInt()})
 
 //        THEN
-        assertEquals(sum, 11)
+        assertEquals( 22, a)
+        assertEquals( 11, b)
 
     }
 
